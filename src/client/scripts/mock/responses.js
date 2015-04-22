31988933fcc0613ff7be2a7be24b0dd6dc7ff4f8
Meesayen <gvg.fede@gmail.com>
2015-04-22 23:56:36 +0100

    Removed useless files. JSHint maxlen set to 120.

--- src/client/scripts/mock/responses.js
+++
@@ -1,8 +0,0 @@
-import * as base from './routes/base';
-
-export default {
-  '/api/testing': base.testing,
-  '/api/one': base.one,
-  '/api/last': base.last,
-  '/api/awesome-list-data': base.awesomeList
-};
