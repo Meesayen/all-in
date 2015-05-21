4be073b67c7c4d89e652e58de4c4cd5c05c16be2
Meesayen <gvg.fede@gmail.com>
2015-05-21 20:18:18 +0100

    Reworked player actions toolbar in a radial menu fashion. Upgraded to Polymer 0.9. Fixed issue with System.js and es6-module-loader. Fixed server issue while managing blinds.

---
+++ src/client/components/mobile/actionpad.js
@@ -0,0 +1,44 @@
+class ActionPad {
+  constructor() {
+  }
+
+  ready() {
+    // this.classList.add('hidden');
+  }
+
+  onHandleClick(e) {
+    e.target.classList.toggle('triggered');
+  }
+  _onCheck() {
+    this.fire('action', { type: 'check' });
+    this.$.handle.classList.remove('triggered');
+  }
+  _onCall() {
+    this.fire('action', { type: 'call' });
+    this.$.handle.classList.remove('triggered');
+  }
+  _onRaise() {
+    // TODO: implement raise dialog and use its output
+    // let amount = this.raiseSlider.value;
+    let amount = 100;
+    this.$.handle.classList.remove('triggered');
+    this.fire('action', { type: 'raise', amount: amount });
+  }
+  _onFold() {
+    this.fire('action', { type: 'fold' });
+    this.$.handle.classList.remove('triggered');
+  }
+}
+
+
+// Maybe with babel Stage 0 and Class properties this will
+// be less ugly
+ActionPad.prototype.is = 'ai-action-pad';
+ActionPad.prototype.properties = {
+  state: {
+    type: String,
+    reflectToAttribute: true
+  }
+};
+
+document.registerElement('ai-action-pad', Polymer.Class(ActionPad.prototype));
